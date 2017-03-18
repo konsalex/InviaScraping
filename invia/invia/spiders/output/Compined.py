2d363b1bc09ba5c83f75e5906cbb1131db1f7042
konsalex <kostakos14@gmail.com>
2017-03-18 11:47:46 +0200

    Not und

--- invia/invia/spiders/output/Compined.py
+++ invia/invia/spiders/output/Compined.py
@@ -1,4 +1,6 @@
 import glob
+import os
+
 
 interesting_files = glob.glob("*.csv") 
 
@@ -12,3 +14,15 @@ with open('compined.csv','wb') as fout:
                 header_saved = True
             for line in fin:
                 fout.write(line)
+
+
+os.remove("Corfu.csv")
+os.remove("Kreta.csv")
+os.remove("Corfuordered.csv")
+os.remove("Kretaordered.csv")
+os.remove("Rhodos.csv")
+os.remove("Rhodosordered.csv")
+os.remove("Zakynthos.csv")
+os.remove("Zakynthosordered.csv")
+
+
